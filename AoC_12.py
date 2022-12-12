7e45092c7be73ecc8064ab566306037a85aef489
Rob Curtis <rob.curtis@graylog.com>
2022-12-12 10:17:34 -0500

    Day 12 working

--- AoC_12.py
+++ AoC_12.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 input = open('input_12', 'r')
 lines = input.readlines()
 
@@ -23,6 +25,42 @@ for y in range(0,len(lines)):
         else:
             elev[y][x] = ord(lines[y][x]) - ord('a')
 
-print('Start at [{}] - End at [{}]'.format(start, end))
-
 path[end['y']][end['x']] = 0
+to_check = deque()
+to_check.append(end)
+
+while len(to_check) > 0:
+    checking = to_check.popleft()
+    # Check up
+    if checking['y'] - 1 >= 0 \
+    and elev[checking['y'] - 1][checking['x']] >= elev[checking['y']][checking['x']] - 1 \
+    and path[checking['y'] - 1][checking['x']] > path[checking['y']][checking['x']] + 1:
+        path[checking['y'] - 1][checking['x']] = path[checking['y']][checking['x']] + 1
+        to_check.append({'x': checking['x'], 'y': checking['y'] - 1})
+    # Check down
+    if checking['y'] + 1 < len(elev) \
+    and elev[checking['y'] + 1][checking['x']] >= elev[checking['y']][checking['x']] - 1 \
+    and path[checking['y'] + 1][checking['x']] > path[checking['y']][checking['x']] + 1:
+        path[checking['y'] + 1][checking['x']] = path[checking['y']][checking['x']] + 1
+        to_check.append({'x': checking['x'], 'y': checking['y'] + 1})
+    # Check left
+    if checking['x'] - 1 >= 0 \
+    and elev[checking['y']][checking['x'] - 1] >= elev[checking['y']][checking['x']] - 1 \
+    and path[checking['y']][checking['x'] - 1] > path[checking['y']][checking['x']] + 1:
+        path[checking['y']][checking['x'] - 1] = path[checking['y']][checking['x']] + 1
+        to_check.append({'x': checking['x'] - 1, 'y': checking['y']})
+    # Check right
+    if checking['x'] + 1 < len(elev[0]) \
+    and elev[checking['y']][checking['x'] + 1] >= elev[checking['y']][checking['x']] - 1 \
+    and path[checking['y']][checking['x'] + 1] > path[checking['y']][checking['x']] + 1:
+        path[checking['y']][checking['x'] + 1] = path[checking['y']][checking['x']] + 1
+        to_check.append({'x': checking['x'] + 1, 'y': checking['y']})
+
+print(path[start['y']][start['x']])
+
+min_path = path[start['y']][start['x']]
+for i in range(0, len(elev)):
+    for j in range(0, len(elev[i])):
+        if elev[i][j] == 0 and path[i][j] < min_path:
+            min_path = path[i][j]
+print(min_path)
