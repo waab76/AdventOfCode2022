87b89fa18a993297258e44ed81d8e625cf1373cd
Rob Curtis <rob.curtis@graylog.com>
2022-12-15 11:25:03 -0500

    Complete day 15

--- AoC_15.py
+++ AoC_15.py
@@ -6,7 +6,6 @@ input = open('input_15','r')
 lines = input.readlines()
 
 sensors = []
-
 for line in lines:
     sensor = {}
     match = pattern.match(line)
@@ -15,5 +14,33 @@ for line in lines:
     beacon_x = int(match.group(3))
     beacon_y = int(match.group(4))
     sensor['range'] = abs(sensor['x'] - beacon_x) + abs(sensor['y'] - beacon_y)
-    # print('Sensor at [{},{}], beacon at [{},{}]\n  Range is {}'.format(sensor['x'],sensor['y'],beacon_x,beacon_y,sensor['range']))
     sensors.append(sensor)
+
+target_row = 2000000
+covered = set()
+for sensor in sensors:
+    y_diff = abs(sensor['y'] - target_row)
+    if y_diff <= sensor['range']:
+        cover_left = sensor['x'] - (sensor['range'] - y_diff)
+        cover_right = sensor['x'] + (sensor['range'] - y_diff)
+        for i in range(cover_left, cover_right + 1):
+            covered.add(i)
+print(len(covered))
+
+for y in range(0,4000001):
+    x = 0
+    coverage = []
+    for sensor in sensors:
+        y_diff = abs(sensor['y'] - y)
+        if y_diff <= sensor['range']:
+            cover_left = sensor['x'] - (sensor['range'] - y_diff)
+            cover_right = sensor['x'] + (sensor['range'] - y_diff)
+            coverage.append((cover_left,cover_right))
+    coverage.sort()
+    while x < 4000001:
+        for range in coverage:
+            if x >= range[0] and x <= range[1]:
+                x = range[1] + 1
+            elif x < range[0]:
+                print(4000000 * x + y)
+                quit()
