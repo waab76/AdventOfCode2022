1c37c8b2635843e08d81af438efb4f92ef8268a6
Rob Curtis <robert.h.curtis@gmail.com>
2022-12-16 07:40:17 -0500

    Initial parsing of graph

---
+++ AoC_16.py
@@ -0,0 +1,18 @@
+import re
+
+input = open('input_16','r')
+lines = input.readlines()
+
+pattern = re.compile('Valve (..) has flow rate=(\d+); tunnels? leads? to valves? (.+)')
+
+valves = {}
+curr_valve = 'AA'
+
+for line in lines:
+    match = pattern.match(line)
+    # print(match.group(0))
+    valve = {}
+    valve['label'] = match.group(1)
+    valve['flow'] = int(match.group(2))
+    valve['neighbors'] = match.group(3).split(', ')
+    valves[valve['label']] = valve
